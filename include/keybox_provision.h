e28ea4ba2855fd0f46ef912b0900db911570dd4b
Chen Gang G <gang.g.chen@intel.com>
2020-05-29 15:00:23 +0800

    boot: support attestation keybox provisioning
    Tracked-On: OAM-91192
    Signed-off-by: Chen Gang G <gang.g.chen@intel.com>

--- include/keybox_provision.h
+++ include/keybox_provision.h
@@ -17,49 +17,6 @@
 #ifndef _KEYBOX_PROVISION_H_
 #define _KEYBOX_PROVISION_H_
 
-#define MAX_KEYBOX_SIZE                16384
-#define ATTKB_META_BASE_ADDRESS	       10
-#define ATTKB_META_SIGNATURE	       "BARA"
-#define ATTKB_META_SIGNATURE_LENGTH    4
-#define ATTKB_PRESENT_FLAG_BIT         0x1
-#define RPMB_BLOCK_SIZE                256
-
-#pragma pack (1)
-typedef struct {
-	uint8_t  signature[4];
-	uint32_t length;
-	uint32_t revision;
-	uint32_t flag;
-	uint16_t attkb_addr;
-	uint32_t attkb_size;
-	uint8_t  reserved[234]; //to have a total size 256
-} attkb_meta_block_t;
-
-typedef struct {
-	uint8_t  version; // version 1 supports plain and LZMA
-	uint16_t size;    // decompressed size excluding this header
-	struct {
-		uint8_t compressed : 1; //0- uncompressed 1- LZMA
-		uint8_t encrypted  : 1;
-		uint8_t reserved   : 6;
-	} format;
-	uint8_t flag;
-	uint16_t reserved;
-} attkb_header_t;
-
-typedef struct {
-	uint16_t      format_version; //use 1 for now
-	uint16_t      blob_sz;
-	uint8_t       iv[GCM_IV_SIZE];
-	uint8_t       tag[GCM_TAG_SIZE];
-	uint8_t       blob[0];
-} attkb_cipher_blob_t;
-
-typedef struct {
-	attkb_header_t header;
-	attkb_cipher_blob_t cipher_blob;
-} encrypted_attkb_t;
-#pragma pack ()
-
 EFI_STATUS flash_keybox(VOID *data, UINTN size);
+
 #endif
--- libfastboot/keybox_provision.c
+++ libfastboot/keybox_provision.c
@@ -16,202 +16,77 @@
 
 #include <lib.h>
 #include <vars.h>
-#include <security.h>
-#include "rpmb_storage.h"
-#include <aes_gcm.h>
-#include <keybox_provision.h>
+#include <efi.h>
+#include <efilib.h>
+#include <gpt.h>
+#include <log.h>
 
-static struct gcm_key attkb_key = {0};
-static encrypted_attkb_t enc_kb;
-static UINT8 *start_kb_addr = NULL;
+#define MAX_KEYBOX_SIZE    16384
+#define KB_HEAD_OFFSET     0
 
-static EFI_STATUS prepare_aad(encrypted_attkb_t *enc_kb, UINT8 *iv, UINTN kb_sz)
-{
-	memset(enc_kb, 0, sizeof(encrypted_attkb_t));
-	enc_kb->header.version = 1;
-	enc_kb->header.size = sizeof(attkb_cipher_blob_t) + kb_sz;
-	enc_kb->header.format.encrypted = 1;
-	enc_kb->cipher_blob.format_version = 1;
-	enc_kb->cipher_blob.blob_sz = kb_sz;
-	return memcpy_s(enc_kb->cipher_blob.iv, sizeof(enc_kb->cipher_blob.iv), iv, GCM_IV_SIZE);
-}
-
-static EFI_STATUS encrypt_keybox(UINT8 *kb_data, UINTN kb_sz,UINT8 *out, UINTN *out_sz)
-{
-	EFI_STATUS ret = EFI_SUCCESS;
-	UINT8 iv[GCM_IV_SIZE];
-	UINTN tag_offset;
-	int rc;
-
-	ret = generate_random_numbers(iv, sizeof(iv));
-	if (EFI_ERROR(ret)) {
-		efi_perror(ret, L"Failed to generate IV");
-		return ret;
-	}
-
-	ret = prepare_aad(&enc_kb, iv, kb_sz);
-	if (EFI_ERROR(ret))
-		return ret;
-
-	rc = aes_256_gcm_encrypt(&attkb_key,
-			iv, GCM_IV_SIZE,
-			&enc_kb, sizeof(enc_kb),
-			kb_data, kb_sz,
-			out, out_sz);
-
-	if (rc == AES_GCM_NO_ERROR) {
-		//update tag
-		tag_offset = *out_sz - GCM_TAG_SIZE;
-		ret = memcpy_s(enc_kb.cipher_blob.tag, sizeof(enc_kb.cipher_blob.tag),
-					   out + tag_offset, GCM_TAG_SIZE);
-	} else {
-		ret = EFI_ABORTED;
-		efi_perror(ret, L"Failed to encrypt keybox");
-	}
-
-	return ret;
-
-}
-
-static EFI_STATUS prepare_attkb_metadata_block(attkb_meta_block_t *data, UINTN kb_size)
-{
-	EFI_STATUS ret;
-	ret = memcpy_s(data->signature, sizeof(data->signature),
-				   ATTKB_META_SIGNATURE, ATTKB_META_SIGNATURE_LENGTH);
-	if (EFI_ERROR(ret))
-		return ret;
-	data->length = RPMB_BLOCK_SIZE;
-	data->revision = 0;
-	data->flag |= ATTKB_PRESENT_FLAG_BIT;
-	data->attkb_addr = ATTKB_META_BASE_ADDRESS + 1;
-	data->attkb_size = kb_size;
-	return EFI_SUCCESS;
-}
-
-static EFI_STATUS write_attkb_data_real(UINT8 *start_kb_addr, UINTN write_sz)
-{
-	EFI_STATUS ret = EFI_SUCCESS;
-#ifndef RPMB_STORAGE
-	(void)start_kb_addr;
-	(void)write_sz;
-	error(L"please enable RPMB_STORAGE first!");
-	ret = EFI_ABORTED;
-#else
-	UINT8 rpmb_buffer[RPMB_BLOCK_SIZE];
-	RPMB_RESPONSE_RESULT rpmb_result;
-	UINT16 blk_addr = ATTKB_META_BASE_ADDRESS;
-	UINT16 blk_cnt;
-	UINTN remain;
-	UINT8 *data_addr = start_kb_addr;
-	UINT8 rpmb_key[RPMB_KEY_SIZE];
-
-	blk_cnt = write_sz / RPMB_BLOCK_SIZE;
-	remain = write_sz % RPMB_BLOCK_SIZE;
-	ret = get_rpmb_key(rpmb_key);
-	if (EFI_ERROR(ret)) {
-		efi_perror(ret, L"Failed to get keybox data");
-		goto exit;
-	}
-	ret = write_rpmb_data(NULL, blk_cnt, blk_addr, data_addr, rpmb_key, &rpmb_result);
-	debug(L"ret=%d, rpmb_result=%d", ret, rpmb_result);
-	if (EFI_ERROR(ret)) {
-		efi_perror(ret, L"Failed to write keybox data");
-		goto exit;
-	}
-	blk_addr += blk_cnt;
-	data_addr = data_addr + (blk_cnt * RPMB_BLOCK_SIZE);
-
-	if (remain) {
-		memset(rpmb_buffer, 0, RPMB_BLOCK_SIZE);
-		ret = memcpy_s(rpmb_buffer, sizeof(rpmb_buffer), data_addr, remain);
-		if (EFI_ERROR(ret))
-			goto exit;
-		ret = write_rpmb_data(NULL, 1, blk_addr, data_addr, rpmb_key, &rpmb_result);
-		debug(L"ret=%d, rpmb_result=%d", ret, rpmb_result);
-		if (EFI_ERROR(ret)) {
-			efi_perror(ret, L"Failed to write keybox data");
-			goto exit;
-		}
-	}
-exit:
-	//clear the rpmb key
-	memset(rpmb_key, 0, sizeof(rpmb_key));
-	barrier();
-#endif
-	return ret;
-}
-
-static EFI_STATUS write_attkb_data_sim(UINT8 *start_kb_addr, UINTN write_sz)
-{
-	EFI_STATUS ret = EFI_SUCCESS;
-	UINT16 blk_addr = ATTKB_META_BASE_ADDRESS;
-	UINT32 byte_offset = blk_addr * RPMB_BLOCK_SIZE;
-
-	ret = simulate_write_rpmb_data(byte_offset, start_kb_addr, write_sz);
-	debug(L"ret=%d", ret);
-	if (EFI_ERROR(ret)) {
-		efi_perror(ret, L"Failed to write keybox data");
-		return ret;
-	}
-
-	return ret;
-}
+typedef struct keybox_header {
+	uint8_t magic[12];
+	uint32_t size;
+	uint8_t keybox[0];
+} keybox_header_t;
 
 EFI_STATUS flash_keybox(VOID *data, UINTN size)
 {
 	EFI_STATUS ret = EFI_SUCCESS;
-	UINTN total_sz;
-	UINTN write_sz;
-	UINTN kb_meta_sz;
-	UINT8 *cipher_blob;
-	UINTN cipher_blob_sz;
-	UINT8 *addr;
+	uint64_t partoffset;
+	struct gpt_partition_interface gpart;
+	keybox_header_t kb_header =
+		{
+			.magic = "MAGICKEYBOX",
+			.size = size,
+		};
+
+	if (!data) {
+		error(L"keybox data is NULL!");
+		return EFI_INVALID_PARAMETER;
+	}
 
 	if (size > MAX_KEYBOX_SIZE) {
 		error(L"keybox size exceeded limit");
 		return EFI_INVALID_PARAMETER;
 	}
 
-	total_sz = sizeof(attkb_meta_block_t) + sizeof(encrypted_attkb_t) + size + GCM_TAG_SIZE;
-	start_kb_addr = AllocatePool(total_sz);
-        if (!start_kb_addr)
-                return EFI_OUT_OF_RESOURCES;
-
-	cipher_blob = start_kb_addr + sizeof(attkb_meta_block_t) + sizeof(encrypted_attkb_t);
-	ret = encrypt_keybox(data, size, cipher_blob, &cipher_blob_sz);
+	ret = gpt_get_partition_by_label(L"teedata", &gpart, LOGICAL_UNIT_USER);
 	if (EFI_ERROR(ret)) {
-		error(L"keybox encryption failure!");
+		efi_perror(ret, L"Partition teedata not found");
 		goto exit;
 	}
 
-	addr = start_kb_addr;
-	kb_meta_sz = sizeof(encrypted_attkb_t) + size;
-	ret = prepare_attkb_metadata_block((attkb_meta_block_t *)addr, kb_meta_sz);
-	if (EFI_ERROR(ret))
-		goto exit;
+	partoffset = gpart.part.starting_lba * gpart.bio->Media->BlockSize;
 
-	addr = addr + sizeof(attkb_meta_block_t);
-	ret = memcpy_s(addr, sizeof(encrypted_attkb_t), &enc_kb, sizeof(encrypted_attkb_t));
-	if (EFI_ERROR(ret))
+	ret = uefi_call_wrapper(
+				gpart.dio->WriteDisk,
+				5,
+				gpart.dio,
+				gpart.bio->Media->MediaId,
+				partoffset + KB_HEAD_OFFSET,
+				sizeof(keybox_header_t),
+				(void *)&kb_header);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"Could not write keybox header to disk.");
 		goto exit;
+	}
 
-	write_sz = sizeof(attkb_meta_block_t) + sizeof(encrypted_attkb_t) + size;
-#ifndef RPMB_SIMULATE
-	if (is_eom_and_secureboot_enabled())
-		ret = write_attkb_data_real(start_kb_addr, write_sz);
-	else
-		ret = write_attkb_data_sim(start_kb_addr, write_sz);
-#else
-	ret = write_attkb_data_sim(start_kb_addr, write_sz);
-#endif
-	if (EFI_ERROR(ret))
-		error(L"keybox write to rpmb failure!");
+	ret = uefi_call_wrapper(
+				gpart.dio->WriteDisk,
+				5,
+				gpart.dio,
+				gpart.bio->Media->MediaId,
+				sizeof(keybox_header_t) + partoffset + KB_HEAD_OFFSET,
+				size,
+				data);
+	if (EFI_ERROR(ret)) {
+		efi_perror(ret, L"Could not write keybox to disk.");
+		goto exit;
+	}
+
 exit:
-	//clean up the keybox plaintext and encrypted data including metadata
-	//information before free the memory
 	memset(data, 0, size);
-	memset(start_kb_addr, 0, total_sz);
 	barrier();
-	FreePool(start_kb_addr);
 	return ret;
 }
